ae3335230cbdbbe3c5d0059eef6b1ae5ac18d26b
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-05-10 13:41:58 +0300

    add styles prbar

--- watertrack/src/components/progressBar/ProgressBar.jsx
+++ watertrack/src/components/progressBar/ProgressBar.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import s from "./ProgressBar.module.css"
+import React from "react";
+import s from "./ProgressBar.module.css";
 const ProgressBar = ({ percent }) => {
-
   return (
     <div>
       <div>
@@ -9,19 +8,22 @@ const ProgressBar = ({ percent }) => {
         <div className={s.progressBar}>
           <p className={s.statsStart}>0%</p>
           <p className={s.statsEnd}>100%</p>
-          <div className={s.track} style={{ width: percent > 100 ? 100 + "%" : percent + "%" }}>
+          <div
+            className={s.track}
+            style={{ width: percent > 100 ? 100 + "%" : percent + "%" }}
+          >
             <div className={s.circle}>
-              {percent > 7 && percent < 95 &&
-                <div className={s.percentage}>
-                <p >{percent > 100 ? 100 + "%" : percent + "%"}</p>
-              </div>
-              }
+              {percent > 7 && percent < 95 && (
+                <p className={s.percentage}>
+                  {percent > 100 ? 100 + "%" : percent + "%"}
+                </p>
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
