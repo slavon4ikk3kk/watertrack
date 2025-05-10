b9cd9962b7cd5b3e4088641d3714493fe704674f
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-05-10 14:05:11 +0300

    fix button leftpart

--- watertrack/src/components/progressBar/ProgressBar.jsx
+++ watertrack/src/components/progressBar/ProgressBar.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import s from "./ProgressBar.module.css";
 const ProgressBar = ({ percent }) => {
   return (
-    
-      <div style={{flexGrow: "1"}}>
-        <p className={s.today}>Today</p>
-        <div className={s.progressBar}>
-          <p className={s.statsStart}>0%</p>
-          <p className={s.statsEnd}>100%</p>
-          <div
-            className={s.track}
-            style={{ width: percent > 100 ? 100 + "%" : percent + "%" }}
-          >
-            <div className={s.circle}>
-              {percent > 7 && percent < 95 && (
-                <p className={s.percentage}>
-                  {percent > 100 ? 100 + "%" : percent + "%"}
-                </p>
-              )}
-            </div>
+    <div style={{ flexGrow: "1", paddingBottom: "35px" }}>
+      <p className={s.today}>Today</p>
+      <div className={s.progressBar}>
+        <p className={s.statsStart}>0%</p>
+        <p className={s.statsEnd}>100%</p>
+        <div
+          className={s.track}
+          style={{ width: percent > 100 ? 100 + "%" : percent + "%" }}
+        >
+          <div className={s.circle}>
+            {percent > 7 && percent < 95 && (
+              <p className={s.percentage}>
+                {percent > 100 ? 100 + "%" : percent + "%"}
+              </p>
+            )}
           </div>
         </div>
       </div>
-
+    </div>
   );
 };
 
