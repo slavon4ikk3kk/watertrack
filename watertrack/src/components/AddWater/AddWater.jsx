a7161a167c52acb1698f6034fe713b14cfc72068
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-05-17 13:59:22 +0300

    add flatpicr

--- watertrack/src/components/AddWater/AddWater.jsx
+++ watertrack/src/components/AddWater/AddWater.jsx
@@ -1,38 +1,38 @@
-import React, {useState} from 'react'
-import s from "./AddWater.module.css"
-import images from "../../assets/index.js"
-import TimePicker from '../TimePicker/TimePicker.jsx'
+import React, { useState } from "react";
+import s from "./AddWater.module.css";
+import images from "../../assets/index.js";
+import TimePicker from "../TimePicker/TimePicker.jsx";
 const AddWater = () => {
-  const [time, setTime] = useState(null)
+  const [time, setTime] = useState(null);
   return (
     <div className={s.mainModal}>
       <div className={s.head}>
         <p className={s.title}>Add Water</p>
         <img src={images.close}></img>
       </div>
-      <p style={{marginBottom: "12px"}}>Choose a value:</p>
+      <p style={{ marginBottom: "12px" }}>Choose a value:</p>
       <p>Amount of water:</p>
       <div className={s.amountWater}>
-       <button className={s.controlButton}>
-        <img src={images.minus}></img>
-       </button>
-       <div className={s.amount}>
-        <p className={s.amountP}>50ml</p>
-       </div>
-       <button className={s.controlButton}>
-        <img src={images.plus}></img>
-       </button>
+        <button className={s.controlButton}>
+          <img src={images.minus}></img>
+        </button>
+        <div className={s.amount}>
+          <p className={s.amountP}>50ml</p>
+        </div>
+        <button className={s.controlButton}>
+          <img src={images.plus}></img>
+        </button>
       </div>
-      <p style={{marginBottom: "12px"}}>Recording time:</p>
-      <TimePicker value={time} onChange={(selected) => setTime(selected[0])} />
+      <p style={{ marginBottom: "12px" }}>Recording time:</p>
+      <TimePicker value={time} onChange={setTime} />
       <p className={s.p}>Enter the value of the water used:</p>
-      <input type='number'></input>
+      <input type="number"></input>
       <div className={s.save}>
-         <p>50ml</p>
-         <button className={s.buttonSave}>Save</button>
+        <p>50ml</p>
+        <button className={s.buttonSave}>Save</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddWater
+export default AddWater;
--- watertrack/src/components/TimePicker/TimePicker.jsx
+++ watertrack/src/components/TimePicker/TimePicker.jsx
@@ -1,23 +1,35 @@
-import "flatpickr/dist/themes/material_blue.css";
-import React from "react";
-import Flatpickr from "flatpickr";
-import "flatpickr/dist/themes/material_blue.css";
-
+import React, { useRef, useEffect } from "react";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/material_blue.css"; // можеш вибрати інший стиль
+import s from "./TimePicker.module.css";
 const TimePicker = ({ value, onChange }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const fp = flatpickr(inputRef.current, {
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: "H:i",
+      time_24hr: true,
+      defaultDate: value,
+      onChange: (selectedDates) => {
+        onChange(selectedDates[0]);
+      },
+    });
+
+    return () => {
+      fp.destroy();
+    };
+  }, [onChange, value]);
+
   return (
-    <div style={{ maxWidth: 200, margin: "20px auto" }}>
-      <Flatpickr
-        options={{
-          enableTime: true,
-          noCalendar: true,
-          dateFormat: "H:i",
-          time_24hr: true,
-        }}
-        value={value}
-        onChange={onChange}
-        className="custom-input"
-      />
-    </div>
+    <input
+      type="text"
+      ref={inputRef}
+      className={s.customInput}
+      placeholder="Оберіть час"
+      readOnly
+    />
   );
 };
 
